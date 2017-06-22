4926bc57581b4f67b332db9f213e343572a9ead6
Rimvydas <rimvel@users.noreply.github.com>
2017-06-22 16:31:43 +0300

    Update AddCustomWindowsVM.ps1

--- AddCustomWindowsVM.ps1
+++ AddCustomWindowsVM.ps1
@@ -1,3 +1,13 @@
+# https://www.linkedin.com/in/rimvydas/
+#
+# This scripts does the following:
+# - Login to Azure Account
+# - Gets and selects (uncomment) subscription
+# - Lists available VM sizes for your subscription and selected region (uncomment)
+# - Creates resource group and VNet (with subnets) using defined names
+# - Sets VM size and creates new (windows) VM using your custom VHD disk (must be uploaded as fixed VHD, as Page Blob to your storage account)
+# - Deletes created resource group and it's content
+
 Login-AzureRmAccount
 Get-AzureRmSubscription
 
